121fb5b6c74897888bdcff0aa27c046ed6871a09
Billyrr24 <Billyrr24@gmail.com>
2025-07-09 14:09:12 -0500

    Create index.js

---
+++ types-bundle/index.js
@@ -0,0 +1,30 @@
+export const typesBundle = {
+  "spec": {
+    "vitreus-power-plant": {
+      "rpc": {},
+      "types": {},
+      "typesAlias": {},
+      "instances": {},
+      "versions": [
+        {
+          "minmax": [
+            0,
+            null
+          ],
+          "types": {}
+        }
+      ],
+      "signedExtensions": {
+        "CheckNonZeroSender": {},
+        "CheckSpecVersion": {},
+        "CheckTxVersion": {},
+        "CheckGenesis": {},
+        "CheckMortality": {},
+        "CheckNonce": {},
+        "CheckWeight": {},
+        "ChargeTransactionPayment": {},
+        "CheckEnergyFee": {}
+      }
+    }
+  }
+};
