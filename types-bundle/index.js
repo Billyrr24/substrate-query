b2020626c58a8499f0356d94eeb21bacbf9b2a5a
Billyrr24 <Billyrr24@gmail.com>
2025-07-09 13:25:22 -0500

    Add files via upload

---
+++ types-bundle/index.js
@@ -0,0 +1,22 @@
+export const typesBundle = {
+  "spec": {
+    "vitreus-power-plant": {
+      "alias": {},
+      "types": {},
+      "rpc": {},
+      "instances": {},
+      "signedExtensions": {
+        "CheckNonZeroSender": {},
+        "CheckSpecVersion": {},
+        "CheckTxVersion": {},
+        "CheckGenesis": {},
+        "CheckMortality": {},
+        "CheckNonce": {},
+        "CheckWeight": {},
+        "ChargeTransactionPayment": {},
+        "CheckEnergyFee": {}
+      },
+      "version": 213
+    }
+  }
+};
